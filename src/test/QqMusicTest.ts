b246706001600ec95140921e37cabc08ca190954
xiyuan_fengyu <xiyuan_fengyu@163.com>
2018-05-18 00:34:31 +0800

    阻止图片加载的方式重写 下载图片的方式重写 response返回结果拦截方案验证成功

--- src/test/QqMusicTest.ts
+++ src/test/QqMusicTest.ts
@@ -12,17 +12,27 @@ import {FileUtil} from "../common/util/FileUtil";
     const page = await browser.newPage();
     await PuppeteerUtil.defaultViewPort(page);
     await PuppeteerUtil.setImgLoad(page, false);
-    PuppeteerUtil.onResponse(page,
-        "https://c.y.qq.com/v8/fcg-bin/fcg_play_single_song.fcg\\?songmid=.*", async response => {
-            const text = await response.text();
-            FileUtil.write(__dirname + "/003LxmX246aRC7/fcg_play_single_song.json", PuppeteerUtil.jsonInJsonp(text));
-        });
-    PuppeteerUtil.onResponse(page,
-        "https://c.y.qq.com/v8/fcg-bin/fcg_play_single_song.fcg\\?songmid=.*", async response => {
-            const text = await response.text();
-            FileUtil.write(__dirname + "/003LxmX246aRC7/fcg_play_single_song.json", PuppeteerUtil.jsonInJsonp(text));
-        });
-    await page.goto("https://y.qq.com/n/yqq/song/003LxmX246aRC7.html");
+
+
+    // 多次回调
+    // PuppeteerUtil.onResponse(page,
+    //     "https://c.y.qq.com/v8/fcg-bin/fcg_play_single_song.fcg\\?songmid=.*", async response => {
+    //         const text = await response.text();
+    //         FileUtil.write(__dirname + "/003LxmX246aRC7/fcg_play_single_song.json", PuppeteerUtil.jsonInJsonp(text));
+    //     });
+    // await page.goto("https://y.qq.com/n/yqq/song/003LxmX246aRC7.html");
+
+
+    // 只触发一次调掉
+    // const waitInfo = PuppeteerUtil.onceResponce(page,
+    //     "https://c.y.qq.com/v8/fcg-bin/fcg_play_single_song.fcg\\?songmid=.*");
+    // await page.goto("https://y.qq.com/n/yqq/song/003LxmX246aRC7.html");
+    // await waitInfo.then(async response => {
+    //     const text = await response.text();
+    //     FileUtil.write(__dirname + "/003LxmX246aRC7/fcg_play_single_song.json", PuppeteerUtil.jsonInJsonp(text));
+    // })
+
+
     // const downloadImgRes = await PuppeteerUtil.downloadImg(page, "body > div.main > div.mod_data > span.data__cover > img", __dirname);
 
 })();
