62ed29144fbcd582ddad161540645ec5111454b5
xiyuan_fengyu <xiyuan_fengyu@163.com>
2018-07-13 19:19:15 +0800

    修复 序列化 中因为循环引用导致的问题

--- src/common/serialize/Serialize.ts
+++ src/common/serialize/Serialize.ts
@@ -19,35 +19,59 @@ export class SerializableUtil {
     }
 
     static serialize(obj: any) {
-        if (obj == null) return obj;
+        const serializedCaches = [];
+        return this._serialize(obj, serializedCaches, "$");
+    }
 
-        const objCons = obj.constructor;
-        if (objCons == Array) {
-            const arr = [];
-            for (let item of obj) {
-                arr.push(this.serialize(item));
-            }
-            return arr;
+    private static _serialize(obj: any, serializedCaches: any[], path: string) {
+        // console.log(path);
+        if (obj == null || obj == undefined) return obj;
+
+        const objType = typeof obj;
+        if (objType == "string" || objType == "number" || objType == "boolean") {
+            return obj;
         }
-        else if (obj instanceof Object) {
-            let ser: any;
-            let flag = this.isSerialize(objCons);
+
+        for (let serializedCache of serializedCaches) {
+            if (serializedCache.instance == obj) {
+                return "ref(" + serializedCache.path + ")";
+            }
+        }
+
+        serializedCaches.push({
+            instance: obj,
+            path: path
+        });
+
+        let res;
+        const objConstructor = obj.constructor;
+        if (objType == "function" || !objConstructor) {
+            res = {};
+        }
+        else if (objConstructor == Array) {
+            res = [];
+            (obj as Array<any>).forEach((value, index) => {
+                res.push(this._serialize(value, serializedCaches, path + "[" + index + "]"));
+            });
+        }
+        else {
+            let flag = this.isSerialize(objConstructor);
             if (flag) {
                 if (obj instanceof Serializable) {
-                    ser = obj.serialize();
-                    ser.serializeClassId = objCons.serializeClassId;
+                    res = obj.serialize();
+                    res.serializeClassId = objConstructor.serializeClassId;
                 }
             }
-            if (ser == null) {
-                ser = {};
+
+            if (res == null) {
+                res = {};
                 for (let field of Object.keys(obj)) {
-                    ser[field] = this.serialize(obj[field]);
+                    res[field] = this._serialize(obj[field], serializedCaches, path + "[" + JSON.stringify(field) + "]");
                 }
-                if (flag) ser.serializeClassId = objCons.serializeClassId;
+                if (flag) res.serializeClassId = objConstructor.serializeClassId;
             }
-            return ser;
         }
-        else return obj;
+        return res || {};
     }
 
     private static isExtendsFromSerializable(clazz) {
@@ -60,41 +84,62 @@ export class SerializableUtil {
         }
         return false;
     }
-    
+
     static deserialize(obj: any): any {
+        return this._deserialize(obj, {}, obj, "$");
+    }
+
+    private static _deserialize(obj: any, deserializedCaches: any, root: any, path: string): any {
         if (obj == null) return obj;
 
-        if (obj.constructor == Array) {
-            const arr = [];
-            for (let item of obj) {
-                arr.push(this.deserialize(item));
-            }
-            return arr;
+        const objType = typeof obj;
+        if (objType == "number" || objType == "boolean") {
+            return obj;
         }
-        else if (obj instanceof Object) {
+        else if (objType == "string") {
+            const str = obj as string;
+            if (str.startsWith("ref(") && str.endsWith(")")) {
+                const refPath = str.substring(4, str.length - 1);
+                let deserializedCache = deserializedCaches[refPath];
+                if (deserializedCache === undefined) {
+                    const $ = root;
+                    deserializedCache = deserializedCaches[refPath] = eval(refPath);
+                }
+                return deserializedCache;
+            }
+            else return obj;
+        }
+
+        let res = null;
+        if (obj.constructor == Array) {
+            res = deserializedCaches[path] = [];
+            (obj as Array<any>).forEach((value, index) => {
+                res.push(this._deserialize(value, deserializedCaches, root, path + "[" + index + "]"));
+            });
+        }
+        else {
             const serializeClassId = obj.serializeClassId;
             const serializeClass = serializeClassId ? this.serializables[serializeClassId] : null;
             if (serializeClass) {
                 if (this.isExtendsFromSerializable(serializeClass)) {
-                    return new serializeClass(obj);
+                    res = deserializedCaches[path] = new serializeClass(obj);
                 }
                 else {
-                    const deser: any = new serializeClass();
+                    res = deserializedCaches[path] = new serializeClass();
                     for (let field of Object.keys(obj)) {
-                        if (field != "serializeClassId") deser[field] = this.deserialize(obj[field]);
+                        if (field != "serializeClassId") res[field] =
+                            this._deserialize(obj[field], deserializedCaches, root, path + "[" + JSON.stringify(field) + "]");
                     }
-                    return deser;
                 }
             }
             else {
-                const deser: any = {};
+                res = deserializedCaches[path] = {};
                 for (let field of Object.keys(obj)) {
-                    deser[field] = this.deserialize(obj[field]);
+                    res[field] = this._deserialize(obj[field], deserializedCaches, root, path + "[" + JSON.stringify(field) + "]");
                 }
-                return deser;
             }
         }
-        else return obj;
+        return res;
     }
 
 }
---
+++ src/test/SerializeTest.ts
@@ -0,0 +1,55 @@
+import {BitSet} from "../common/util/BitSet";
+import {SerializableUtil} from "../common/serialize/Serialize";
+
+class A {
+    paraA = "aaa";
+    random = parseInt((Math.random() * 10000).toFixed());
+    b: B;
+}
+
+class B {
+    paraB = 123;
+    random = parseInt((Math.random() * 10000).toFixed());
+    a: A;
+}
+
+class C {
+    private a: A;
+    private b: B;
+    bitSet: BitSet;
+    paramNum = 12;
+    paramBoolean = true;
+    paramString = "test";
+    paramObj = {
+        aa: null,
+        bb: null,
+        bitSet: null
+    };
+    paramArray: any[] = [
+        1,
+        "2",
+        false
+    ];
+
+    constructor() {
+        this.a = new A();
+        this.b = new B();
+        this.a.b = this.b;
+        this.b.a = this.a;
+        this.bitSet = new BitSet(2 << 10);
+        this.bitSet.set(8, 1);
+        this.bitSet.set(16, 1);
+        this.paramObj.aa = this.a;
+        this.paramObj.bb = this.b;
+        this.paramObj.bitSet = this.bitSet;
+        this.paramArray.push(this.a);
+        this.paramArray.push(this.b);
+        this.paramArray.push(this.bitSet);
+    }
+}
+
+const c = new C();
+const serC = SerializableUtil.serialize(c);
+console.log(JSON.stringify(serC, null, 4));
+const deserC = SerializableUtil.deserialize(serC);
+console.log(deserC);
