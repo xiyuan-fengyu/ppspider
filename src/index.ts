fcb8390ab45e4b4d851237e687cefdb5a6206adb
xiyuan_fengyu <xiyuan_fengyu@163.com>
2019-05-10 14:07:48 +0800

    添加 NetworkTracing，用于生成 timeline-viewer 可视化数据

--- src/index.ts
+++ src/index.ts
@@ -41,7 +41,7 @@ export {AbsQueue} from "./spider/queue/AbsQueue";
 export {Queue} from "./spider/queue/Queue";
 export {NoneWorkerFactory} from "./spider/worker/NoneWorkerFactory";
 export {WorkerFactory} from "./spider/worker/WorkerFactory";
-export {NetworkTracing} from "./puppeteer/NetworkTracing";
+export {NetworkTracing, PageRequests, PageRequest} from "./puppeteer/NetworkTracing";
 export {
     ResponseListener,
     DownloadImgError,
