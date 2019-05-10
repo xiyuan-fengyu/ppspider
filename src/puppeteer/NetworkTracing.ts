dae8d41b2cbd6f7bfdd27df6d7997d3952fe3cb5
xiyuan_fengyu <xiyuan_fengyu@163.com>
2019-05-10 13:05:34 +0800

    添加 NetworkTracing，用于生成 timeline-viewer 可视化数据

---
+++ src/puppeteer/NetworkTracing.ts
@@ -0,0 +1,122 @@
+import {Page} from "puppeteer";
+
+export type PageRequest = {
+    id: string,
+    url: string,
+    method: string,
+    time: number,
+    response?: {
+        status: number,
+        contentType: string,
+        contentLength: number,
+        fromCache: boolean,
+        fromServiceWorker: boolean,
+        time: number
+    },
+    endTime: number
+    success: boolean
+}
+
+export type PageRequests = {
+    time: number,
+    requests: PageRequest[]
+}
+
+export class NetworkTracing {
+
+    private startTime: number;
+
+    private requestMap: any = {};
+
+    private page: Page;
+
+    private onRequest: (...args: any[]) => void;
+
+    private onResponse: (...args: any[]) => void;
+
+    private onRequestFinished: (...args: any[]) => void;
+
+    private onRequestFailed: (...args: any[]) => void;
+
+    constructor(page: Page) {
+        page.on("request", this.onRequest = event => {
+            const _requestId = event["_requestId"];
+            this.requestMap[_requestId] = {
+                id: _requestId,
+                url: event.url(),
+                method: event.method,
+                time: new Date().getTime()
+            };
+        });
+
+        page.on("response", this.onResponse = event => {
+            const response = this.requestMap[event["_request"]["_requestId"]].response = {
+                status: event["_status"],
+                contentType: event["_headers"]["content-type"],
+                contentLength: event["_headers"]["content-length"],
+                fromCache: event["_fromDiskCache"],
+                fromServiceWorker: event["_fromServiceWorker"],
+                time: new Date().getTime()
+            };
+            if (response.contentLength == null) {
+                event.buffer().then(buffer => response.contentLength = buffer.length);
+            }
+            else {
+                response.contentLength = parseInt(response.contentLength);
+            }
+        });
+
+        const requestEnd = (event, success) => {
+            const requestInfo = this.requestMap[event["_requestId"]];
+            requestInfo.endTime = new Date().getTime();
+            requestInfo.success = success;
+        };
+        page.on("requestfinished", this.onRequestFinished = event => {
+            requestEnd(event, true);
+        });
+        page.on("requestfailed", this.onRequestFailed = event => {
+            requestEnd(event, false);
+        });
+
+        this.page = page;
+        this.startTime = new Date().getTime();
+    }
+
+    requests(): PageRequests  {
+        this.page.removeListener("request", this.onRequest);
+        this.page.removeListener("response", this.onResponse);
+        this.page.removeListener("requestfinished", this.onRequestFinished);
+        this.page.removeListener("requestfailed", this.onRequestFailed);
+
+        const tracing = [];
+        for (let key of Object.keys(this.requestMap)) {
+            tracing.push(this.requestMap[key]);
+        }
+        tracing.sort((o1, o2) => o1.time - o2.time);
+        return {
+            time: this.startTime,
+            requests: tracing
+        };
+    }
+
+    static requestsToTraceEvents(pageRequests: PageRequests) {
+        const startTime = pageRequests.time;
+        const ts = time => {
+            return (time - startTime) * 1000;
+        };
+        const traceEvents: any[] = [];
+        traceEvents.push({"tid":0, "ts":0, "ph":"I","cat":"disabled-by-default-devtools.timeline","name":"TracingStartedInBrowser","args":{"data":{"frameTreeNodeId":0,"persistentIds":true,"frames":[{"url":"about:blank","name":"","processId":1}]}}});
+        for (let request of pageRequests.requests) {
+            traceEvents.push({"pid":1,"tid":1,"ts":ts(request.time),"ph":"I","cat":"devtools.timeline","name":"ResourceSendRequest","args":{"data":{"requestId":request.id,"url":request.url,"requestMethod":request.method}}});
+            if (request.response) {
+                traceEvents.push({"pid":1,"tid":1,"ts":ts(request.response.time),"ph":"I","cat":"devtools.timeline","name":"ResourceReceiveResponse","args":{"data":{"requestId":request.id,"statusCode":request.response.status,"mimeType":request.response.contentType,"encodedDataLength":request.response.contentLength,"fromCache":request.response.fromCache,"fromServiceWorker":request.response.fromServiceWorker,"timing":{"requestTime":ts(request.time) / 1000000,"sendStart":0,"receiveHeadersEnd":request.response.time - request.time}}}});
+            }
+            traceEvents.push({"pid":1,"tid":1,"ts":ts(request.endTime),"ph":"I","cat":"devtools.timeline","name":"ResourceFinish","args":{"data":{"requestId":request.id, "didFail": !request.success, "decodedBodyLength": 0}}})
+        }
+        traceEvents.sort((o1, o2) => o1.ts - o2.ts);
+        return {
+            traceEvents: traceEvents
+        };
+    }
+
+}
