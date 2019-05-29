056ea382e0bbf58bc417d9e156315f8748b3e5b5
xiyuan_fengyu <xiyuan_fengyu@163.com>
2019-05-29 23:59:55 +0800

    代理方案研究

--- src/test/playground/PuppeteerPageProxyTest.ts
+++ src/test/playground/PuppeteerPageProxyTest.ts
@@ -32,64 +32,67 @@ ResStream.prototype.getBody = function () {
     const browser = await launch({
         headless: false,
         devtools: true,
-        // args: [ '--proxy-server=127.0.0.1:3000' ]
+        args: [ '--proxy-server=127.0.0.1:3000' ]
     });
     const page = await browser.newPage();
     await page.setViewport({width: 1920, height: 1080});
-    await page.setRequestInterception(true);
+    // await page.setRequestInterception(true);
+    // page.on("request", (req: Request) => {
+    //     if (req.url().startsWith("http")) {
+    //         const reqInfo = {
+    //             id: req["_requestId"],
+    //             url: req.url(),
+    //             method: req.method(),
+    //             headers: req.headers(),
+    //             postData: req.postData(),
+    //             proxy: "http://127.0.0.1:2007"
+    //         };
+    //
+    //         const options = url.parse(reqInfo.url);
+    //         options["method"] = reqInfo.method;
+    //         options["headers"] = reqInfo.headers;
+    //
+    //         const resHandler = (proxyRes: IncomingMessage) => {
+    //             const stream = new ResStream();
+    //             proxyRes.pipe(stream as any);
+    //             stream.on("finish", () => {
+    //                 let body = stream.getBody();
+    //                 if ((proxyRes.headers["content-type"] || "").toLowerCase().indexOf("charset=utf-8") > -1) {
+    //                     body = body.toString("utf-8");
+    //                 }
+    //                 req.respond({
+    //                     status: proxyRes.statusCode,
+    //                     headers: proxyRes.headers as any,
+    //                     body: body
+    //                 });
+    //             });
+    //             stream.on("error", (err: Error) => {
+    //                 req.abort("failed");
+    //             });
+    //         };
+    //
+    //         const proxy = reqInfo.proxy;
+    //         let proxyReq;
+    //         if (options.protocol == "http:") {
+    //             options["agent"] = new HttpProxyAgent(proxy);
+    //             proxyReq = http.request(options, resHandler);
+    //         }
+    //         else {
+    //             options["agent"] = new HttpsProxyAgent(proxy);
+    //             proxyReq = https.request(options, resHandler);
+    //         }
+    //
+    //         if (reqInfo.postData) {
+    //             proxyReq.write(reqInfo.postData);
+    //         }
+    //         proxyReq.end();
+    //     }
+    //     else {
+    //         req.continue();
+    //     }
+    // });
     page.on("request", (req: Request) => {
-        if (req.url().startsWith("http")) {
-            const reqInfo = {
-                id: req["_requestId"],
-                url: req.url(),
-                method: req.method(),
-                headers: req.headers(),
-                postData: req.postData(),
-                proxy: "http://127.0.0.1:2007"
-            };
-
-            const options = url.parse(reqInfo.url);
-            options["method"] = reqInfo.method;
-            options["headers"] = reqInfo.headers;
-
-            const resHandler = (proxyRes: IncomingMessage) => {
-                const stream = new ResStream();
-                proxyRes.pipe(stream as any);
-                stream.on("finish", () => {
-                    let body = stream.getBody();
-                    if ((proxyRes.headers["content-type"] || "").toLowerCase().indexOf("charset=utf-8") > -1) {
-                        body = body.toString("utf-8");
-                    }
-                    req.respond({
-                        status: proxyRes.statusCode,
-                        headers: proxyRes.headers as any,
-                        body: body
-                    });
-                });
-                stream.on("error", (err: Error) => {
-                    req.abort("failed");
-                });
-            };
-
-            const proxy = reqInfo.proxy;
-            let proxyReq;
-            if (options.protocol == "http:") {
-                options["agent"] = new HttpProxyAgent(proxy);
-                proxyReq = http.request(options, resHandler);
-            }
-            else {
-                options["agent"] = new HttpsProxyAgent(proxy);
-                proxyReq = https.request(options, resHandler);
-            }
-
-            if (reqInfo.postData) {
-                proxyReq.write(reqInfo.postData);
-            }
-            proxyReq.end();
-        }
-        else {
-            req.continue();
-        }
+        console.log(req.url());
     });
     page.on("response", resp => {
         if (resp.fromCache()) {
